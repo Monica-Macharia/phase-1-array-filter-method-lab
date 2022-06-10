bae8e2de1b0abeff94fbaa0b29f85bac404719ff
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-10 23:11:47 +0300

    commit tree 129bdde50b75cc5e93a3b954aa28325c9e4d1a68

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(driversName, string){
+    return driversName.filter(item => item.toLowerCase() === string.toLowerCase())
+
+}
+//console.log(findMatching(drivers, "Bobby"))
+
+function fuzzyMatch(drivers,letter){
+    return drivers.filter(function(driver){
+        return driver[0].toUpperCase() === letter[0].toUpperCase()
+    })
+       
+
+}
+//console.log (fuzzyMatch(drivers, 'S')); 
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName(arrayOfDrivers,string){
+   let name = arrayOfDrivers.filter(function(item){
+        return item.name === string
+    })
+    return name;
+}
+console.log (matchName(drivers, "Bobby"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
